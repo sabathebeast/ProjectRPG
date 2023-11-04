9d3ae14ab59e8c21b93f60a3d57df754b22e6381
Sinkó Csaba <sinko.csabaa@gmail.com>
2023-11-04 15:07:00 +0100

    getting rid of some magic number

--- map.h
+++ map.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "raylib.h"
 
+static const int tileSize = 16;
+static const int tileRow = 70;
+static const int tileColumn = 70;
+
 class Map
 {
 public:
 	Map();
 	~Map();
 
-	void loadMap(int arr[64][64]);
-	void drawMap(Texture tex0, Texture tex1, Texture tex2);
+	int mapWidth = tileRow * tileSize;
+	int mapHeight = tileColumn * tileSize;
+	int mapTileSize = tileSize;
+
+	void loadMap(int arr[tileRow][tileColumn]);
+	void drawMap(Texture tex0, Texture tex1, Texture tex2, Vector2 direction);
 
 private:
-	int map[64][64];
+	int map[tileRow][tileColumn];
 };
