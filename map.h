5ff4738e637f8cda8f99e7495e621118e4ea494f
Csaba Sinkó <sinko.csabaa@gmail.com>
2024-01-26 19:41:12 +0100

    map reads from file

--- map.h
+++ map.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "raylib.h"
+#include <fstream>
+#include <vector>
 
 static const int tileSize = 16;
 static const int tileRow = 70;
@@ -19,6 +21,10 @@ public:
 	void drawMap(Texture tex0, Texture tex1, Texture tex2, Vector2 direction, unsigned char levelExploreMap[tileRow][tileColumn]);
 	unsigned char levelZeroExploreMap[tileRow][tileColumn] = { 0 };
 	unsigned char levelOneExploreMap[tileRow][tileColumn] = { 0 };
-	unsigned char map[tileRow][tileColumn];
+	unsigned char baseMap[tileRow][tileColumn] = { 0 };
+	unsigned char map[tileRow][tileColumn] = { 0 };
 private:
+	std::ofstream outputFile;
+	std::ifstream inputFile;
+	std::vector<std::string> inputData;
 };
